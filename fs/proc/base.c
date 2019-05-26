d736c2c8ada8b749c8267d839fdf212211864673
Michael Benedict <michaelbt@live.com>
2019-05-26 22:55:01 +1000

    fs: disable samsung lmkd integration
    Signed-off-by: Michael Benedict <michaelbt@live.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -2892,7 +2892,9 @@ static const struct pid_entry tgid_base_stuff[] = {
 	REG("cmdline",    S_IRUGO, proc_pid_cmdline_ops),
 	ONE("stat",       S_IRUGO, proc_tgid_stat),
 	ONE("statm",      S_IRUGO, proc_pid_statm),
+#ifdef CONFIG_ZSWAP
 	ONE("statlmkd",      S_IRUGO, proc_pid_statlmkd),
+#endif
 	REG("maps",       S_IRUGO, proc_pid_maps_operations),
 #ifdef CONFIG_NUMA
 	REG("numa_maps",  S_IRUGO, proc_pid_numa_maps_operations),
@@ -3283,7 +3285,9 @@ static const struct pid_entry tid_base_stuff[] = {
 	REG("cmdline",   S_IRUGO, proc_pid_cmdline_ops),
 	ONE("stat",      S_IRUGO, proc_tid_stat),
 	ONE("statm",     S_IRUGO, proc_pid_statm),
+#ifdef CONFIG_ZSWAP
 	ONE("statlmkd",      S_IRUGO, proc_pid_statlmkd),
+#endif
 	REG("maps",      S_IRUGO, proc_tid_maps_operations),
 #ifdef CONFIG_PROC_CHILDREN
 	REG("children",  S_IRUGO, proc_tid_children_operations),
